e770f963bdbfa87610bd58a776f8847409e3350f
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2017-12-18 22:18:14 +0530

    Create Display hello.c

---
+++ Begineer level/Display hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{ 
+	int n;
+               int i;
+              printf("Enter the number of times to display\n");
+              scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("Hello\n");
+	}
+	return 0;
+}	
