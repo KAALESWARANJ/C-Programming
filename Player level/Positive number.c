85a12bfff2ad8ab9bbc82f837683d7b9d8abd397
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2017-12-19 11:36:07 +0530

    Create Positive number.c

---
+++ Player level/Positive number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main ( )
+
+{
+
+int num;
+
+printf("Enter the number");
+
+scanf("%d",&num);
+
+if (num<0)
+
+printf("The number is negative") ;
+
+else
+
+printf("The number is positive");
+
+}
