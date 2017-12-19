e296cde6789d149d9459c200ccf9dc2362560eb4
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2017-12-19 21:00:59 +0530

    Update Number is positive,negative or zero.c

--- Begineer level/Number is positive,negative or zero.c
+++ Begineer level/Number is positive,negative or zero.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main(void)
 {
 	int a;
 	printf("Enter the number:\n");
 	scanf("%d",&a);
-	if(a<0)	
-	printf("Negative");
-	else if(a>0)
+	if(a>0)	
 	printf("Positive");
+	else if(a<0)
+	printf("Negative");
 	else
 	printf("zero");
 	return 0;
