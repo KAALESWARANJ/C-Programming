b1e7729c43e5b3917d1a5a93a30eaa1c1bac8c24
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2017-12-20 23:52:29 +0530

    Update Character occurence.c

--- codekata/Character occurence.c
+++ codekata/Character occurence.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{ 
+	int a,i;
+	char b[100];
+	int count[26];
+	gets(b);
+	a=strlen(b);
+	for(i=0;i<26;i++)
+	{
+	count[i]=0;
+	}
+	for(i=0;i<a;i++)
+	{
+	if(b[i]>=97&&b[i]<=122)
+	count[b[i]-97]++;
+	else
+	count[b[i]-65]++;
+	}
+	for(i=0;i<26;i++)
+	{
+	if(count[i]!=0)
+	printf("%c-%d\n",(i+97),count[i]);
+	}
+	return 0;
+}
+	
+	
 
