72e895240ceb91bed05ec18c54392429db39ca28
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-01-03 21:01:59 +0530

    Create validatestring.c

---
+++ codekata/validatestring.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a[10];
+	gets(a);
+	int i,n;
+	n=strlen(a);
+           int count=0;
+           int count1=0;
+	for(i=0;i<n;i++)
+	{
+	if(a[i]>='0' && a[i]<='9')
+	{
+	count++;	
+	}
+	else
+	{
+	count1++;	
+	}
+	}
+	if(count!=0)
+	{
+		printf("TRUE");
+	}
+	else
+	{
+		printf("False");
+	}
+	return 0;
+}
