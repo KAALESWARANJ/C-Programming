1d82f421219effd7b6cd4dc0966aec74c9f73134
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-01-09 16:39:40 +0530

    Create primeornot.c

---
+++ codekata/primeornot.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,i,count=0;
+	scanf("%d\n",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			count=1;
+			break;
+		}
+	}
+	if(count==0)
+            printf(" prime");
+            else
+            printf(" not prime");
+            return 0;
+}
