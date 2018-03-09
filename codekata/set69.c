8f17d58ebaf1d53a7b46193df665169b94c5e821
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-09 22:37:38 +0530

    Create set69.c

---
+++ codekata/set69.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[100],i,j,val,n,max;
+	 scanf("%d\n",&n);
+	 for(i=0;i<=n;i++)
+           {
+	scanf("%d\t",&a[i]);
+	
+            }
+       max=a[0];
+       for(i=0;i<=n;i++)
+         {
+            if(a[i]>max)
+          {
+       	max=a[i];
+           }
+         }
+    printf("%d",max);
+    return 0;
+	
+}
+    
