ce33ad2773752a04e96892f98525c7e0bcd541f5
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-09 09:34:49 +0530

    Update set122.c

--- codekata/set122.c
+++ codekata/set122.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	 int a;
-	 scanf("%d\n",&a);
-	 if(a%2==0)
+	 int b;
+	 scanf("%d\n",&b);
+	 if(b%2==0)
 	 {
-	 	printf("Even");
+	 	printf("Even number");
 	 }
 	 else
 	 {
-	    printf("Odd");	
+	    printf("Odd number");	
 	 }
 	return 0;
 }
