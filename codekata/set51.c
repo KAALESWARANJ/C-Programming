bb345a358a2265b3dae72104f0ca2aa3b3759851
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-09 12:43:21 +0530

    Create set51.c

---
+++ codekata/set51.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[100],i,n,k;
+	scanf("%d\n",&n);
+            gets(a);
+            scanf("%d\n",&k);
+            for(i=0;i<k;i++)
+            {
+            	printf("%s\n",a);
+            }
+}
