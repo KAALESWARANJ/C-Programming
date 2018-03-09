c4dc2210d41a076e451af5acd3e910ade7c21a89
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-09 19:04:08 +0530

    Create set54.c

---
+++ codekata/set54.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,n1,n2;
+	scanf("%d\n",&a);
+	scanf("%d\n",&n1);
+	scanf("%d\n",&n2);
+		if(a>=n1 && a<=n2)
+		{
+			printf("Yes");
+		}
+		else
+		{
+			printf("No");
+		}
+	
+}
