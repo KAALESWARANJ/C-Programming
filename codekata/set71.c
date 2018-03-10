682be5d3c1456b7cc34aa0b2fcdf8036dad58da9
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-10 13:52:35 +0530

    Create set71.c

---
+++ codekata/set71.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+    char a[100]; 
+    int k,i;
+    scanf("%d\n",&k);
+    gets(a);
+               for(i=0;i<k;i++)
+    	{
+    	   	printf("%c",a[i]);
+    	}
+    
+	return 0;
+}
