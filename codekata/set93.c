6fcebb4d60f67fc2a0d3e81bc915e87d1d9d8728
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-10 21:43:02 +0530

    Create set93.c

---
+++ codekata/set93.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b;
+	scanf("%d %d\n",&a,&b);
+	printf("%d\n",a/b);
+	int c,d;
+	scanf("%d %d\n",&c,&d);
+	printf("%d\n",c%d);
+            unsigned long int a1,b1;
+	scanf("%lu %lu\n",&a1,&b1);
+	printf("%lu\n",a1/b1);
+	unsigned long int c1,d1;
+	scanf("%lu %lu \n",&c1,&d1);
+	printf("%lu\n",c1%d1);
+ 
+}
