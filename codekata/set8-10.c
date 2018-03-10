c22f6bcc241efa12c0b580a7d43dcf5973ea4ad5
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-10 19:38:12 +0530

    Create set8-10.c

---
+++ codekata/set8-10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int n,t,f=1;
+  scanf("%d",&n);
+  t=n;
+  while(t)
+  {
+      t=t/10;
+      f = f*10;
+  }
+  while(f>1)
+  {
+      f = f/10;
+      if((n/f)%2!=0)
+      {
+      printf("%d ",n/f);
+      }
+      n=n % f;
+  }
+
+  return 0;
+}
