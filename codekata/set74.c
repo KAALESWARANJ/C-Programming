026f48d2eaf718657b6399decfd96a2ac4c8e6cd
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-10 14:13:40 +0530

    Create set74.c

---
+++ codekata/set74.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+	int m,n;
+	scanf("%d %d",&m,&n);
+            scanf("%d/n",&n);
+            if((m+n)%2==0)
+            {
+            	printf("Even");
+            }
+            else
+            {
+            	printf("odd");
+            }
+           
+}
